b33e4a414fbaa75a20cc176bb4ed066ab8b873bb
Demmyjsx <ademolaabds@gmail.com>
2025-09-02 13:20:31 +0100

    animated the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import {ScrollTrigger, SplitText} from "gsap/all"
 import gsap from "gsap"
-
+import NavBar from "./components/NavBar"
+import Hero from "./components/Hero"
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 export default function App() {
   return (
-    <div className="flex-center h-[100vh] ">
-         <h1 className="text-3xl text-indigo-300">this tailwind works</h1>
-    </div>
-   
+    <main>
+       <NavBar />
+       <Hero />
+       {console.log(`i day work o`)}
+    </main>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,49 @@
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react';
+import {navLinks}  from '../constants/index'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function NavBar() {
+  const containerRef = useRef();
+
+  useGSAP(()=>{
+    const navTween = gsap.timeline({
+      scrollTrigger : {
+        trigger: 'nav',
+        start: 'bottom top',
+        scrub: true, 
+      }
+    });
+
+    navTween.fromTo('nav', {
+      backgroundColor: 'transparent'
+    }, {
+      backgroundColor: "#00000050",
+      backdropFilter: 'blur(10px)', 
+      duration:1,
+      ease: 'power1.inOut',
+    });
+  }, { scope: containerRef }); 
+
+  return (
+    <nav ref={containerRef}>
+        <div>
+            <a href="#home" className='flex items-center gap-2'>
+               <img src="/images/logo.png" alt="" />
+                <p>Velvet Pour</p>
+            </a>
+
+            <ul>
+                {navLinks.map((link)=>(
+                  <li key={link.id}>
+                    <a href={`#${link.id}`}>{link.title}</a> 
+                  </li>
+                ))}
+            </ul>
+        </div>
+    </nav>
+  )
+}
