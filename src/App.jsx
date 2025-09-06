e2976d9fdefefc54702246b10c9af5a875d48c68
Demmyjsx <ademolaabds@gmail.com>
2025-09-06 14:27:21 +0100

    finishing touch

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Cocktails from "./components/Cocktails"
 import About from "./components/About"
 import Art from "./components/Art"
 import Menu from "./components/Menu"
-import Contant from "./components/Contact"
+import Contact from "./components/Contact"
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 export default function App() {
@@ -16,9 +16,9 @@ export default function App() {
        <Hero />
        <Cocktails /> 
        <About />
-       {/* <Art /> */}
+       <Art />
        <Menu />
-       <Contant />
+       <Contact />
     </main>
   )
 }
